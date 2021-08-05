3ff0ee663c8743eb0dc6b3cfb572542dede6db53
unknown <you@example.com>
2021-08-05 15:35:13 +0300

    add procfile

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,6 @@ app.use('/api/products', productRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-const MONGO_URI = process.env.MONGO_URI
 
-app.listen(MONGO_URI, PORT, () => console.log(`Server running on port ${PORT}`))
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
