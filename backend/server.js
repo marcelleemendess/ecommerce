a902fd755f51d2fe24e4b3f5dae49ab8c8c3c2be
unknown <you@example.com>
2021-08-05 15:24:29 +0300

    ssss

--- backend/server.js
+++ backend/server.js
@@ -13,4 +13,6 @@ app.use('/api/products', productRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+const MONGO_URI = process.env.MONGO_URI
+
+app.listen(MONGO_URI, PORT, () => console.log(`Server running on port ${PORT}`))
