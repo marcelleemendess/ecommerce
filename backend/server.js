a650c7eb634f3c91be485421ab08f230fdf077a0
unknown <you@example.com>
2021-08-05 18:12:54 +0300

    add cors

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,15 @@ require("dotenv").config();
 const express = require('express');
 const connectDB = require('./config/db')
 const productRoutes = require('./routes/productRoutes');
+const cors = require('cors')
 
 connectDB();
 
 const app = express();
 
 app.use(express.json());
+
+app.use(cors())
 
 app.use('https://marcelle-ecommerce.herokuapp.com/api/products', productRoutes);
 
